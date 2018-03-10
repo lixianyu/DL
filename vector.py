28d9370268d3cae079d4e90ed9eeeb1feac95c55
lixianyu <lbonus@163.com>
2018-03-10 13:17:40 +0800

    Check in.

--- vector.py
+++ vector.py
@@ -14,25 +14,26 @@ class Vector(object):
         except TypeError:
             raise TypeError('The coordinates must be an iterable')
 
-
     def __str__(self):
         return 'Vector: {}'.format(self.coordinates)
-
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
 
     def plus(self, v):
         new_coordinates = [x+y for x,y in zip(self.coordinates, v.coordinates)]
-        return new_coordinates
+        # return new_coordinates
+        return Vector(new_coordinates)
 
     def minus(self, v):
         new_coordinates = [x-y for x,y in zip(self.coordinates, v.coordinates)]
-        return new_coordinates
+        # return new_coordinates
+        return Vector(new_coordinates)
 
     def times_scalar(self, c):
         new_coordinates = [c*x for x in self.coordinates]
-        return new_coordinates
+        # return new_coordinates
+        return Vector(new_coordinates)
 
     def magnitude(self):
         # he = 0
